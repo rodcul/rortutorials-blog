34f82bee2518097b5caaac532961bd5042c2f165
Rodney Cullen <rodcul@gmail.com>
2013-11-23 20:14:04 +0100

    tut3 - Routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
 	def index
-		
-	end
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
